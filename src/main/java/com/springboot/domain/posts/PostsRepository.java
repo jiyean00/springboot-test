d8e94e939501cdbe39978377d0b773693f18c1f6
jiyean00 <shinjiyeon19@gmail.com>
2022-03-25 15:47:40 +0900

    4장까지

---
+++ src/main/java/com/springboot/domain/posts/PostsRepository.java
@@ -0,0 +1,14 @@
+package com.springboot.domain.posts;
+
+import com.springboot.domain.posts.Posts;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface PostsRepository extends JpaRepository<Posts, Long> {
+
+    @Query("SELECT p FROM Posts p ORDER BY p.id DESC")
+    List<Posts> findAllDesc();
+}
